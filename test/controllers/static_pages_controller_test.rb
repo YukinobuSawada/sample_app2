96b7380d7c419bed5267ca335a61ebc201e0234b
vagrant <vagrant@localhost.localdomain>
2020-12-30 13:21:37 +0000

    3-5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+  	get root_url
+  	assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -9,6 +15,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+  end
+
+  test "should get contact" do
+  	get static_pages_contact_url
+  	assert_response :success
   end
 
 end
