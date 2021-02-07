b1d799fdf8a84e0d701249179476910305cf7f25
vagrant <spw.kyoka@gmail.com>
2021-02-07 14:55:10 +0000

    6.2.9 migration

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -37,9 +37,13 @@ class UserTest < ActiveSupport::TestCase
         	assert_not @user.valid?, "#{valid_address.inspect} should be valid"
         end
     end
+
     test "email addresses should be unique" do
     	duplicate_user = @user.dup
+    	duplicate_user.email = @user.email.upcase
     	@user.save
     	assert_not duplicate_user.valid?
     end
+
+
 end
