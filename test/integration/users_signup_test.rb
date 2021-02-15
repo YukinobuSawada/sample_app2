e050653dab08fad36b9f50eda6420303c3836768
vagrant <spw.kyoka@gmail.com>
2021-02-15 14:57:53 +0000

    users_signup_test

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+	test "invalid signup information" do
+		get signup_path
+		assert_no_difference'User.count'do
+		post users_path,params:{user:{name:"",
+			email:}}
+	end
+end
