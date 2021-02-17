40d25ab6c74a101a794d2acac9461482c372bb08
vagrant <spw.kyoka@gmail.com>
2021-02-17 07:45:47 +0000

    flash test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,7 +4,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	test "invalid signup information" do
 		get signup_path
 		assert_no_difference'User.count'do
-		post users_path,params:{user:{name:"",
+		post signup_path,params:{user:{name:"",
 			email:"user@invalid",
 			password:"foo",
 			password_confirmation: "bar"}}
@@ -12,7 +12,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	assert_template 'users/new'
 	assert_select 'div#error_explanation'
 	assert_select 'div.alert'
-	assert_select 'alert-danger'
+	assert_select 'div.alert-danger'
+	assert_select 'form[action="/signup"]'
+	assert_not flash.empty?
 end
 
+test "valid signup information" do
+		get signup_path
+		assert_difference 'User.count',1 do
+			post users_path,params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+                                     end
+
+    follow_redirect!
+    assert_template 'users/show'
+end
 end
